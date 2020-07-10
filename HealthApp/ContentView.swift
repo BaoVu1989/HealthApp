f7fde2efe478727452812ce51c53642cf54f88af
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-10 13:33:07 +0700

    Completed

--- HealthApp/ContentView.swift
+++ HealthApp/ContentView.swift
@@ -25,7 +25,7 @@ struct Home : View {
     
     @State var index = 0 // this is an index for Tab View
     
-    //var columns = Array(repeating: , count: <#T##Int#>)
+    
     
     var body: some View{
         
@@ -90,19 +90,191 @@ struct Home : View {
             .padding(.horizontal)
             .padding(.top,25)
             
-            // This is a DashBoard Grid....
+            // This is a TabDate....
+            ZStack{
+             TabDate()
+                .opacity(self.index == 0 ? 1 : 0)
+            TabWeek()
+                .opacity(self.index == 1 ? 1 : 0)
+                
+            TabMonth()
+                .opacity(self.index == 2 ? 1 : 0)
+            }
             
             
             
-            Spacer(minLength: 0)
         }
         .padding(.top)
     }
 }
 
+// This is a TabView of a Date....
+
+struct TabDate: View{
+    
+    var body: some View{
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            ForEach(day_fit_Data, id: \.self){day in
+                
+                HStack(spacing: 20){
+                    
+                    ForEach(day){i in
+                        
+                            VStack{
+                                
+                                HStack{
+                                    Text(i.title)
+                                        .foregroundColor(.white)
+                                        .font(.body)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                    Spacer()
+                                    Image(i.image)
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                        .background(Color.black.opacity(0.07))
+                                    .clipShape(Circle())
+                                }
+                                
+                                Text(i.data)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                HStack{
+                                    Spacer()
+                                    Text(i.suggest)
+                                        .font(.footnote)
+                                        .foregroundColor(.white)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    
+                                }
+                            }.padding()
+                                
+                                .frame(width: UIScreen.main.bounds.width / 2.6, height: UIScreen.main.bounds.width / 2.6)
+                                .background(Color("\(i.image)"))
+                            .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.1), radius: 90, x: 5, y: 5)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+// This is a TabView of a Week....
+
+struct TabWeek: View{
+    
+    var body: some View{
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            ForEach(week_fit_Data, id: \.self){week in
+                
+                HStack(spacing: 20){
+                    
+                    ForEach(week){j in
+                        
+                            VStack{
+                                
+                                HStack{
+                                    Text(j.title)
+                                        .foregroundColor(.white)
+                                        .font(.body)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    Spacer()
+                                    Image(j.image)
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                        .background(Color.black.opacity(0.07))
+                                    .clipShape(Circle())
+                                }
+                                
+                                Text(j.data)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                .fixedSize(horizontal: false, vertical: true)
+                                HStack{
+                                    Spacer()
+                                    Text(j.suggest)
+                                        .font(.footnote)
+                                        .foregroundColor(.white)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    
+                                }
+                            }.padding()
+                                
+                                .frame(width: UIScreen.main.bounds.width / 2.6, height: UIScreen.main.bounds.width / 2.6)
+                                .background(Color("\(j.image)"))
+                            .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.1), radius: 90, x: 5, y: 5)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+// This is a TabView of a Month....
+
+struct TabMonth: View{
+    
+    var body: some View{
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            ForEach(month_fit_Data, id: \.self){month in
+                
+                HStack(spacing: 20){
+                    
+                    ForEach(month){j in
+                        
+                            VStack{
+                                
+                                HStack{
+                                    Text(j.title)
+                                        .foregroundColor(.white)
+                                        .font(.body)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    Spacer()
+                                    Image(j.image)
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                        .background(Color.black.opacity(0.07))
+                                    .clipShape(Circle())
+                                }
+                                
+                                Text(j.data)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                .fixedSize(horizontal: false, vertical: true)
+                                HStack{
+                                    Spacer()
+                                    Text(j.suggest)
+                                        .font(.footnote)
+                                        .foregroundColor(.white)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    
+                                }
+                            }.padding()
+                                
+                                .frame(width: UIScreen.main.bounds.width / 2.6, height: UIScreen.main.bounds.width / 2.6)
+                                .background(Color("\(j.image)"))
+                            .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.1), radius: 90, x: 5, y: 5)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
 // This is a DashBoard Grid Model Data....
 
-struct  Fitness: Identifiable {
+struct  Fitness: Identifiable , Hashable{
     
     var id: Int
     var title: String
@@ -114,24 +286,24 @@ struct  Fitness: Identifiable {
 // This is a daily data....
 var day_fit_Data = [
 
-    Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "124 bpm", suggest: "80-120\nHealthy"),
-    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "7H 20M", suggest: "Deep Sleep/n5h 13m"),
-    Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "583 kcal", suggest: "Daily Goal\n900 kcal"),
-    Fitness(id: 3, title: "Steps", image: "steps", data: "16,741", suggest: "Daily Goal\n20,000 Steps"),
-    Fitness(id: 4, title: "Running", image: "running1", data: "5.3 km", suggest: "Daily Goal\n10 km"),
-    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "15.3 km", suggest: "Daily Goal\n20 km")
+   [Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "124 bpm", suggest: "80-120\nHealthy"),
+    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "7H 20M", suggest: "Deep Sleep\n5h 13m")],
+    [Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "583 kcal", suggest: "Daily Goal\n900 kcal"),
+    Fitness(id: 3, title: "Steps", image: "steps1", data: "16,741", suggest: "Daily Goal\n20,000 Steps")],
+    [Fitness(id: 4, title: "Running", image: "running1", data: "5.3 km", suggest: "Daily Goal\n10 km"),
+    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "15.3 km", suggest: "Daily Goal\n20 km")]
     
 ]
 
 // This is a Weekly data ....
 var week_fit_Data = [
 
-    Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "104 bpm", suggest: "80-120\nHealthy"),
-    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "49H 20M", suggest: "Deep Sleep/n5h 13m"),
-    Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "3583 kcal", suggest: "Daily Goal\n900 kcal"),
-    Fitness(id: 3, title: "Steps", image: "steps", data: "76,741", suggest: "Daily Goal\n20,000 Steps"),
-    Fitness(id: 4, title: "Running", image: "running1", data: "35.3 km", suggest: "Daily Goal\n10 km"),
-    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "115.3 km", suggest: "Daily Goal\n20 km")
+    [Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "104 bpm", suggest: "80-120\nHealthy"),
+    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "49H 20M", suggest: "Deep Sleep\n5h 13m")],
+    [Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "3583 kcal", suggest: "Daily Goal\n900 kcal"),
+    Fitness(id: 3, title: "Steps", image: "steps1", data: "76,741", suggest: "Daily Goal\n20,000 Steps")],
+    [Fitness(id: 4, title: "Running", image: "running1", data: "35.3 km", suggest: "Daily Goal\n10 km"),
+    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "115.3 km", suggest: "Daily Goal\n20 km")]
     
 ]
 
@@ -139,11 +311,11 @@ var week_fit_Data = [
 
 var month_fit_Data = [
 
-    Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "114 bpm", suggest: "80-120\nHealthy"),
-    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "70H 20M", suggest: "Deep Sleep/n5h 13m"),
-    Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "35583 kcal", suggest: "Daily Goal\n900 kcal"),
-    Fitness(id: 3, title: "Steps", image: "steps", data: "160,741", suggest: "Daily Goal\n20,000 Steps"),
-    Fitness(id: 4, title: "Running", image: "running1", data: "150.3 km", suggest: "Daily Goal\n10 km"),
-    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "1500.3 km", suggest: "Daily Goal\n20 km")
+    [Fitness(id: 0, title: "Heart Rate", image: "heart1", data: "114 bpm", suggest: "80-120\nHealthy"),
+    Fitness(id: 1, title: "Sleep", image: "sleep1", data: "70H 20M", suggest: "Deep Sleep\n5h 13m")],
+    [Fitness(id: 2, title: "Energy Burn", image: "energy1", data: "35583 kcal", suggest: "Daily Goal\n900 kcal"),
+    Fitness(id: 3, title: "Steps", image: "steps1", data: "160,741", suggest: "Daily Goal\n20,000 Steps")],
+    [Fitness(id: 4, title: "Running", image: "running1", data: "150.3 km", suggest: "Daily Goal\n10 km"),
+    Fitness(id: 5, title: "Cycling", image: "cycling1", data: "1500.3 km", suggest: "Daily Goal\n20 km")]
     
 ]
